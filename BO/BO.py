13e489586ad6860dcdd719f9211672b4bc32203b
CarlSvejstrup <carlsvejstrup@gmail.com>
2025-02-18 15:16:58 +0100

    Implemented the optimizer for the BO algorithm Added BO to main

---
+++ BO/BO.py
@@ -0,0 +1,54 @@
+from skopt import gp_minimize
+from skopt.space import Integer, Categorical
+import numpy as np
+from torch import nn
+import model as CNNmodel
+
+def BO(
+    Model_class, 
+    dimensions, 
+    dataloader, 
+    val_dataloader, 
+    **optimizer_params,
+    ):
+    
+    """
+    Perform Bayesian Optimization on a given model class.
+
+    Parameters:
+    -----------
+    Model_class : class
+        The class of the model to be optimized.
+    dimensions : list
+        List of dimensions for the hyperparameters to be optimized.
+    dataloader : DataLoader
+        DataLoader for the training data.
+    val_dataloader : DataLoader
+        DataLoader for the validation data.
+    **optimizer_params : dict
+        Additional parameters for the optimizer.
+
+    Returns:
+    --------
+    res : OptimizeResult
+        The optimization result represented as a `OptimizeResult` object.
+    """
+
+    def objective(x):
+        model = CNNmodel(**x)
+
+        train_accs, test_accs = model.train_model(
+            dataloader, epochs=train_epochs, val_dataloader=test_dataloader
+        )
+        test_accs = test_accs[-1]
+        return - test_accs
+
+
+    return gp_minimize(
+        objective, dimensions,
+        n_calls=n_calls, 
+        n_inital_points=n_initial_points,
+        initial_point_generator = initial_point_generator,
+        n_points = n_points,
+    )  #
+
